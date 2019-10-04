01cfbf08643d84ed51219c2057e87ea2f4960f29
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-04 16:30:28 +0300

    final commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+  function sum($sum) {
+  $odd = array_filter($sum = ([1, 7, 6, 100]), function ($val) {
+    return $val % 2 != 0;
+  });
 
-  function sum_odds()
+  foreach ($odd as $val) {
+    echo $val . '<br />';
+  }
+  }
+  sum($sum);
+  echo 'Сумма нечетных чисел: '.array_sum($odd);
